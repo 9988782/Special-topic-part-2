b86f92fe513107779ae7d611061f45a4459fae35
Hamiora_Taurima <hamiora.taurima@hotmail.com>
2019-06-17 14:55:06 +1200

    no message

--- BuildingEscape/Source/BuildingEscape/PositionReport.cpp
+++ BuildingEscape/Source/BuildingEscape/PositionReport.cpp
@@ -2,8 +2,7 @@
 
 
 #include "PositionReport.h"
-#include "GameFramework/Actor.h"
-
+#include "Gameframework/Actor.h"
 // Sets default values for this component's properties
 UPositionReport::UPositionReport()
 {
@@ -11,7 +10,9 @@ UPositionReport::UPositionReport()
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
 
-	// ...
+
+	//FString ObjectName = GetOwner()->GetName();
+	//UE_LOG(LogTemp, Warning, TEXT("Position report for %s"),*ObjectName);
 }
 
 
@@ -20,9 +21,7 @@ void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
 
-	FString ObjectName = GetOwner()->GetName();
-	FString ObjectPos = GetOwner()->GetTransform().GetLocation().ToString();
-	UE_LOG(LogTemp, Warning, TEXT("%s is at %s"), *ObjectName, *ObjectPos);
+	// ...
 	
 }
 
